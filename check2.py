166bfcfd3d5452ba015fc329a5fa347c97129812
Jared Sage <jnsage@gmail.com>
2022-06-12 17:17:32 -0400

    first commit for check2.py

---
+++ check2.py
@@ -0,0 +1,9 @@
+#Fill in Missing years will correct years
+from sqlalchemy import column
+
+
+# Correct title spelling. Weekday > Day_of_week, time > showtime
+# Fill in year column based on date column
+# Drop rows with null title
+# Change Village Eight to Village 8
+# Time column to ISO format
